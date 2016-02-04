f74b691e0f575ce6b19cd4d9de5a1055110547b3
Tom Edgar <edgartl@me.com>
2016-02-04 16:30:06 -0600

    Phase 1 complete.  Image added (static) and two text fields.  The keyboard dismissal is handled with resignFirstResponder.  Also, background view class is changed from UIView to UIControl to react to background tap.

--- Controls/ViewController.swift
+++ Controls/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var nameField: UITextField!
+  @IBOutlet weak var numberField: UITextField!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,14 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  @IBAction func textFieldDoneEditing(sender: UITextField) {
+    sender.resignFirstResponder()
+  }
+  
+  @IBAction func backgroundTap(sender: UIControl) {
+    nameField.resignFirstResponder()
+    numberField.resignFirstResponder()
+  }
 }
 
