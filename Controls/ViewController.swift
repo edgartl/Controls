d49d457e492f694c3906530b0be396ef84e81be8
Tom Edgar <edgartl@me.com>
2016-02-04 20:05:44 -0600

    Added files - Initial github commit

--- Controls/ViewController.swift
+++ Controls/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
   @IBOutlet weak var nameField: UITextField!
   @IBOutlet weak var numberField: UITextField!
   @IBOutlet weak var sliderLabel: UILabel!
+  @IBOutlet weak var leftSwitch: UISwitch!
+  @IBOutlet weak var rightSwitch: UISwitch!
+  @IBOutlet weak var doSomethingButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -36,5 +39,48 @@ class ViewController: UIViewController {
     let progress = lroundf(sender.value)
     sliderLabel.text = "\(progress)"
   }
+  @IBAction func switchChanged(sender: UISwitch) {
+    let setting = sender.on
+    leftSwitch.setOn(setting, animated: true)
+    rightSwitch.setOn(setting, animated: true)
+  }
+  
+  @IBAction func toggleControls(sender: UISegmentedControl) {
+    if sender.selectedSegmentIndex == 0 {
+      leftSwitch.hidden = false
+      rightSwitch.hidden = false
+      doSomethingButton.hidden = true
+    } else {
+      leftSwitch.hidden = true
+      rightSwitch.hidden = true
+      doSomethingButton.hidden = false
+    }
+  }
+  @IBAction func buttonTapped(sender: AnyObject) {
+    
+    let actionController = UIAlertController(title: "Are you Sure?", message: nil, preferredStyle: .ActionSheet)
+    
+    let yesAction = UIAlertAction(title: "Yes, I'm Sure", style: .Destructive, handler: {
+      action in let msg = self.nameField.text!.isEmpty ?
+      "You can breathe easy, everything went OK." :
+      "You can breathe easy, \(self.nameField.text)," + "Everything went OK."
+      
+      let alertController = UIAlertController(title: "Something was done", message: msg, preferredStyle: .Alert)
+      let cancelAction = UIAlertAction(title: "Phew", style: .Cancel, handler: nil)
+      alertController.addAction(cancelAction)
+      self.presentViewController(alertController, animated: true, completion: nil)
+    })
+    
+    let noAction = UIAlertAction(title: "No Way!", style: .Cancel, handler: nil)
+    
+    actionController.addAction(yesAction)
+    actionController.addAction(noAction)
+    
+    if let ppc = actionController.popoverPresentationController {
+      ppc.sourceView = sender as? UIView
+      ppc.sourceRect = sender.bounds
+    }
+    
+    presentViewController(actionController, animated: true, completion: nil)
 }
-
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Controls
+
+Sample app from the "Beginning Swift" book.  I've updated the project to use some UIStackViews for Auto Layout.
+
+## Specifics
+
+This project is a sample of controls.  IBOutlets and IBActions are introduced. Keyboard dismissal with resignFirstResponder is handled.  Additionally, UIAlertController is used to present both Alerts and ActionSheets to the user.
